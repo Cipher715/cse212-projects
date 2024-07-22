5dfe675431ef16623b2180bc0c0a4325d35af62c
Cipher715 <atsushi.karino@gmail.com>
2024-07-23 01:36:41 +0900

    W6 code

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -12,25 +12,63 @@ public class Node {
             // Insert to the left
             if (Left is null)
                 Left = new Node(value);
-            else
+            else if (value != Left.Data)
                 Left.Insert(value);
         }
         else {
             // Insert to the right
             if (Right is null)
                 Right = new Node(value);
-            else
+            else if(value != Right.Data)
                 Right.Insert(value);
         }
     }
 
     public bool Contains(int value) {
         // TODO Start Problem 2
-        return false;
+        if (value < Data) {
+            // Insert to the left
+            if (Left is null)
+                return false;
+            else if (value == Left.Data)
+                return true;
+            else 
+               return Left.Contains(value);
+        }
+        else {
+            // Insert to the right
+            if (Right is null)
+                return false;
+            else if(value == Right.Data)
+                return true;
+            else
+                return Right.Contains(value);
+        }
+
     }
 
     public int GetHeight() {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        if (Left == null && Right !=null) {
+            int rightHeight = Right.GetHeight();
+            return 1 + rightHeight;
+        } else if (Right == null && Left != null){
+            int leftHeight = Left.GetHeight();
+            return 1 + leftHeight;
+        } else if (Left == null && Right ==null) {
+            return 1;
+        }
+        else {
+            if (Left != null && Right != null){
+                int rightHeight = Right.GetHeight();
+                int leftHeight = Left.GetHeight();
+                if (leftHeight > rightHeight)
+                    return 1 + leftHeight; // Replace this line with the correct return statement(s)
+                else
+                    return 1 + rightHeight;
+            }
+            else
+                return 1;
+        }
     }
 }
